70288454a6cf1ac6d7c954e96423cc1912d2b928
Andrea <keerakobold@gmail.com>
2021-12-04 13:29:03 -0300

    Commit before deploy

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -4,10 +4,10 @@ import video from '../Media/video.mp4'
 const Home = () => {
     return (
         <section className='section relative flex items-center justify-center text-center' id='home'>
-            <div className='relative z-30 p-5 sm:h-auto text-gray-600 bg-gray-50 bg-opacity-50 rounded-sm -mt-12'>
+            <div className='relative z-30 p-8 sm:h-auto text-gray-600 bg-gray-50 bg-opacity-50 rounded-sm -mt-12'>
                 <h1 className='relative text-3xl'>A title for this website</h1>
                 <p>A subtitle for this website</p>
-                <a href='#about'><button className='btn bg-white rounded-xl py-2 w-40 mt-5 animate-bounce hover:bg-yellow-800 hover:text-yellow-300 transition duration-200 ease-in-out'></button></a>
+                <a href='#about'><button className='btn bg-white rounded-xl p-3 w-40 mt-1 animate-bounce hover:bg-yellow-800 hover:text-yellow-300 transition duration-200 ease-in-out'></button></a>
             </div>
             <video alt='White background - Video by Luna Lovegood from Pexels' className='absolute z-10 object-cover w-auto min-w-full min-h-full h-screen overflow-hidden' autoPlay loop muted src={video}>
                 Your browser does not support the video tag.
